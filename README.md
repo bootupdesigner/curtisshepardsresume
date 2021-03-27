4627f9df2341aa11c3e1bdce4a9add927596149e
bootupdesigner <bootup.itinfo@gmail.com>
2021-03-26 20:37:52 -0400

    This is my first commit. I've added dependencies that will be used in this project. I've also added images that will be included in this design. I've deleted the unnecessary code in the app.js file.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Curtis Shepard's Web Development Resume
---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-slideshow-image/dist/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Curtis Shepard</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
